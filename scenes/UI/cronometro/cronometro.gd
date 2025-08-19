b890f352c340186ab4408f6f87268f20e511f3ae
daniela-luisa <danielaluisaconceicao9063@gmail.com>
2025-08-19 14:16:06 -0300

    programação cronometro feita
    criado as pastas ui/ccronometro, cena cronometro e gd cronometro. (dentro de scenes)

---
+++ scenes/UI/cronometro/cronometro.gd
@@ -0,0 +1,32 @@
+extends Label
+
+@export var auto_start: bool = true   
+var tempo: float = 0.0                
+var rodando: bool = false
+
+func _ready() -> void:
+	text = "00:00"
+	if auto_start:
+		start()
+
+func _process(delta: float) -> void:
+	if not rodando:
+		return
+	tempo += delta
+	_atualiza_texto()
+
+func start() -> void:
+	rodando = true
+
+func pause() -> void:
+	rodando = false
+
+func reset() -> void:
+	rodando = false
+	tempo = 0.0
+	_atualiza_texto()
+
+func _atualiza_texto() -> void:
+	var m := int(tempo) / 60
+	var s := int(tempo) % 60
+	text = "%02d:%02d" % [m, s]
